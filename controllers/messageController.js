9ac5ddc43aeac27f92bf184edfe7557beb954d77
Miguel  de la Mata <sebidelamata@gmail.com>
2024-02-04 15:32:03 -0700

    site skeleton

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,10 +1,12 @@
-const Message = require("../models/message");
+const Message = require("../models/message")
+const User = require("../models/user")
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all Messages.
 exports.message_list = asyncHandler(async (req, res, next) => {
   const allMessages = await Message.find({})
-    .sort({ publish_date: 1 })
+    .sort({ publish_date: -1 })
     .populate("user_id")
     .exec()
 
@@ -33,30 +35,113 @@ exports.message_detail = asyncHandler(async (req, res, next) => {
 
 // Display Message create form on GET.
 exports.message_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Message create GET");
+
+  res.render("message_form", {
+    title: "Create Message",
+    message: null,
+    errors: null
+  })
 });
 
 // Handle Message create on POST.
-exports.message_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Message create POST");
-});
+exports.message_create_post = [
+  body("message", "Message must be at least 1 character")
+  .trim()
+  .isLength({ min: 1 })
+  .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+
+    const testUser = await User.findOne().exec()
+
+    const message = new Message({
+      message: req.body.message,
+      user_id: testUser._id,
+      publish_date: new Date()
+    })
+
+    if(!errors.isEmpty()){
+      res.render("message_form", {
+        message: message,
+        errors: errors.array()
+      })
+      return
+    } else {
+      await message.save()
+      res.redirect("/messages")
+    }
+
+    res.send("NOT IMPLEMENTED: Message create POST");
+  }),
+
+]
 
 // Display Message delete form on GET.
 exports.message_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Message delete GET");
+  const message = await Message.findById(req.params.id).exec()
+
+  if(message === null){
+    res.redirect('/messages')
+  }
+  res.render("message_delete", {
+    message: message
+  })
 });
 
 // Handle Message delete on POST.
 exports.message_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Message delete POST");
+  await Message.findByIdAndDelete(req.body.message_id)
+  res.redirect("/messages")
 });
 
 // Display Message update form on GET.
 exports.message_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Message update GET");
+  const message = await Message.findById(req.params.id)
+  .populate("user_id")
+  .exec()
+
+  if(message === null){
+    const err = new Error("Message not found")
+    err.status = 404
+    return next(err)
+  }
+
+  res.render("message_form", {
+    title: "Update Message",
+    message: message,
+    errors: null,
+  })
 });
 
 // Handle Message update on POST.
-exports.message_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Message update POST");
-});
+exports.message_update_post = [
+  body('message', 'Message must be at least 1 character.')
+  .trim()
+  .isLength({ min: 1 })
+  .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+
+    const message = new Message({
+      message: req.body.message,
+      publish_date: new Date(),
+      user_id: req.body.user_id,
+      _id: req.body._id
+    })
+
+    if(!errors.isEmpty()){
+      res.render("message_form", {
+        title: "Update Message",
+        message: message,
+        errors: errors.array()
+      })
+      return
+    } else {
+      const updatedMessage = await Message.findByIdAndUpdate(req.params.id, message, {})
+      res.redirect(updatedMessage.url)
+    }
+  })
+
+]
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const Message = require("../models/message")
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all Users.
 exports.user_list = asyncHandler(async (req, res, next) => {
@@ -35,30 +36,161 @@ exports.user_detail = asyncHandler(async (req, res, next) => {
 
 // Display User create form on GET.
 exports.user_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: User create GET");
+  res.render("user_form", {
+    title: "Create User",
+    user: null,
+    errors: null
+  })
 });
 
 // Handle User create on POST.
-exports.user_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: User create POST");
-});
+exports.user_create_post = [
+  body("first_name", "First Name must be at least 1 character")
+  .trim()
+  .isLength({ min: 1 })
+  .escape(),
+
+  body("last_name", "Last Name must be at least 1 character")
+  .trim()
+  .isLength({ min: 1 })
+  .escape(),
+
+  body("username", "Username must be an email address")
+  .trim()
+  .isEmail()
+  .escape(),
+
+  body("password", "Password must be at least 1 character")
+  .trim()
+  .isLength({ min: 1 })
+  .escape(),
+
+  body("password_confirm", "Passwords must match")
+  .trim()
+  .custom((value, {req}) => {
+    return value === req.body.password
+  }),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+
+    const user = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      username: req.body.username,
+      password: req.body.password,
+      salt: 10,
+      membership: false,
+      admin: false
+    })
+
+    if(!errors.isEmpty()){
+      res.render("user_form", {
+        title: "Create User",
+        user: user,
+        errors: errors.array()
+      })
+      return
+    } else {
+      await user.save()
+      res.redirect("/users")
+    }
+
+    res.send("NOT IMPLEMENTED: Message create POST");
+  }),
+
+]
 
 // Display User delete form on GET.
 exports.user_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: User delete GET");
+  const user = await User.findById(req.params.id).exec()
+
+  if(user === null){
+    res.redirect("/users")
+  }
+
+  res.render("user_delete", {
+    user: user
+  })
 });
 
 // Handle User delete on POST.
 exports.user_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: User delete POST");
+  await User.findByIdAndDelete(req.body.user_id)
+  res.redirect("/users")
 });
 
 // Display User update form on GET.
 exports.user_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: User update GET");
+  const user = await User.findById(req.params.id)
+  .exec()
+
+  if(user === null){
+    const err = new Error("User not found")
+    err.status = 404
+    return next(err)
+  }
+
+  res.render("user_form", {
+    title: "Update User",
+    user: user,
+    errors: null,
+  })
 });
 
 // Handle User update on POST.
-exports.user_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: User update POST");
-});
+exports.user_update_post = [
+  body("first_name", "First Name must be at least 1 character")
+  .trim()
+  .isLength({ min: 1 })
+  .escape(),
+
+  body("last_name", "Last Name must be at least 1 character")
+  .trim()
+  .isLength({ min: 1 })
+  .escape(),
+
+  body("username", "Username must be an email address")
+  .trim()
+  .isEmail()
+  .escape(),
+
+  body("password", "Password must be at least 1 character")
+  .trim()
+  .isLength({ min: 1 })
+  .escape(),
+
+  body("password_confirm", "Passwords must match")
+  .trim()
+  .custom((value, {req}) => {
+    return value === req.body.password
+  }),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req)
+
+    const user = new User({
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      username: req.body.username,
+      password: req.body.password,
+      salt: 10,
+      membership: false,
+      admin: false,
+      _id: req.body._id
+    })
+
+    if(!errors.isEmpty()){
+      res.render("user_form", {
+        title: "Update User",
+        user: user,
+        errors: errors.array()
+      })
+      return
+    } else {
+      const updatedUser = await User.findByIdAndUpdate(req.params.id, user, {})
+      res.redirect(updatedUser.url)
+    }
+  })
+
+]
